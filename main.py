a777a8b7de13dbcbf2e3687cc6138899c0781681
ex0Rift <daviesnathanael45@gmail.com>
2025-06-03 18:19:26 +0100

    more

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ width , height = 500,500
 
 facing = 'N'
 
+MiniMapObjects=[]
+
 GreenWalls = [
     [3,1]
 ]
 
 Map = [
-    [1,1,1,1,1,1,1,1],
-    [1,0,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,1],
-    [1,1,1,1,1,1,0,1],
-    [1,0,0,0,1,1,0,1],
-    [1,0,1,0,1,1,0,1],
-    [1,0,1,0,0,0,0,1],
-    [1,2,1,0,1,1,0,1],
-    [1,1,1,1,1,1,1,1]
+    [1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,1,1,1,1,1,1,0,1,1,1],
+    [1,1,1,1,1,1,0,1,1,0,1,0,1],
+    [1,0,0,0,1,1,0,1,0,0,1,0,1],
+    [1,0,1,0,1,1,0,1,0,1,1,0,1],
+    [1,0,1,0,0,0,0,1,0,0,1,0,1],
+    [1,2,1,0,1,1,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1]
 ]
 
 #constructors
@@ -112,22 +114,38 @@ def Keybinding(event):
     elif event.keysym == 'Left':
         if facing == 'N':
             facing = 'W'
+            MiniMap.itemconfig(NPointer,state='hidden')
+            MiniMap.itemconfig(WPointer,state='normal')
         elif facing == 'E':
             facing = 'N'
+            MiniMap.itemconfig(EPointer,state='hidden')
+            MiniMap.itemconfig(NPointer,state='normal')
         elif facing == 'S':
             facing = 'E'
+            MiniMap.itemconfig(SPointer,state='hidden')
+            MiniMap.itemconfig(EPointer,state='normal')
         elif facing == 'W':
             facing = 'S'
+            MiniMap.itemconfig(WPointer,state='hidden')
+            MiniMap.itemconfig(SPointer,state='normal')
 
     elif event.keysym == 'Right':
         if facing == 'N':
             facing = 'E'
+            MiniMap.itemconfig(NPointer,state='hidden')
+            MiniMap.itemconfig(EPointer,state='normal')
         elif facing == 'E':
             facing = 'S'
+            MiniMap.itemconfig(EPointer,state='hidden')
+            MiniMap.itemconfig(SPointer,state='normal')
         elif facing == 'S':
             facing = 'W'
+            MiniMap.itemconfig(SPointer,state='hidden')
+            MiniMap.itemconfig(WPointer,state='normal')
         elif facing == 'W':
             facing = 'N'
+            MiniMap.itemconfig(WPointer,state='hidden')
+            MiniMap.itemconfig(NPointer,state='normal')
 
     RefreshScreen()
 
@@ -205,6 +223,154 @@ def RefreshScreen():
             Screen.itemconfig(MLI,state='hidden')
             Screen.itemconfig(MRI,state='hidden')
             Screen.itemconfig(MIDI,state='hidden')
+        
+    '''
+    ############## MiniMap code ##############
+    '''
+    if Map[PlayerLocation[0]][PlayerLocation[1]-1]==1:
+        MiniMap.itemconfig(MiniMapObjects[11],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[11],state='hidden')
+    if Map[PlayerLocation[0]][PlayerLocation[1]+1]==1:
+        MiniMap.itemconfig(MiniMapObjects[13],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[13],state='hidden')
+    if Map[PlayerLocation[0]-1][PlayerLocation[1]]==1:
+        MiniMap.itemconfig(MiniMapObjects[7],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[7],state='hidden')
+    if Map[PlayerLocation[0]+1][PlayerLocation[1]]==1:
+        MiniMap.itemconfig(MiniMapObjects[17],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[17],state='hidden')
+    if Map[PlayerLocation[0]-1][PlayerLocation[1]-1]==1:
+        MiniMap.itemconfig(MiniMapObjects[6],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[6],state='hidden')
+    if Map[PlayerLocation[0]-1][PlayerLocation[1]+1]==1:
+        MiniMap.itemconfig(MiniMapObjects[8],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[8],state='hidden')
+    if Map[PlayerLocation[0]+1][PlayerLocation[1]-1]==1:
+        MiniMap.itemconfig(MiniMapObjects[16],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[16],state='hidden')
+    if Map[PlayerLocation[0]+1][PlayerLocation[1]+1]==1:
+        MiniMap.itemconfig(MiniMapObjects[18],state='normal')
+    else:
+        MiniMap.itemconfig(MiniMapObjects[18],state='hidden')
+    try:
+        if Map[PlayerLocation[0]-2][PlayerLocation[1]-2]==1:
+            MiniMap.itemconfig(MiniMapObjects[0],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[0],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]-2][PlayerLocation[1]-1]==1:
+            MiniMap.itemconfig(MiniMapObjects[1],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[1],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]-2][PlayerLocation[1]]==1:
+            MiniMap.itemconfig(MiniMapObjects[2],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[2],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]-2][PlayerLocation[1]+1]==1:
+            MiniMap.itemconfig(MiniMapObjects[3],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[3],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]-2][PlayerLocation[1]+2]==1:
+            MiniMap.itemconfig(MiniMapObjects[4],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[4],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+2][PlayerLocation[1]-2]==1:
+            MiniMap.itemconfig(MiniMapObjects[20],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[20],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+2][PlayerLocation[1]-1]==1:
+            MiniMap.itemconfig(MiniMapObjects[21],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[21],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+2][PlayerLocation[1]]==1:
+            MiniMap.itemconfig(MiniMapObjects[22],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[22],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+2][PlayerLocation[1]+1]==1:
+            MiniMap.itemconfig(MiniMapObjects[23],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[23],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+2][PlayerLocation[1]+2]==1:
+            MiniMap.itemconfig(MiniMapObjects[24],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[24],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]-1][PlayerLocation[1]-2]==1:
+            MiniMap.itemconfig(MiniMapObjects[5],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[5],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]][PlayerLocation[1]-2]==1:
+            MiniMap.itemconfig(MiniMapObjects[10],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[10],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+1][PlayerLocation[1]-2]==1:
+            MiniMap.itemconfig(MiniMapObjects[15],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[15],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]-1][PlayerLocation[1]+2]==1:
+            MiniMap.itemconfig(MiniMapObjects[9],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[9],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]][PlayerLocation[1]+2]==1:
+            MiniMap.itemconfig(MiniMapObjects[14],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[14],state='hidden')
+    except:
+        pass
+    try:
+        if Map[PlayerLocation[0]+1][PlayerLocation[1]+2]==1:
+            MiniMap.itemconfig(MiniMapObjects[19],state='normal')
+        else:
+            MiniMap.itemconfig(MiniMapObjects[19],state='hidden')
+    except:
+        pass
 
     if facing == 'S':
         if Map[PlayerLocation[0]][PlayerLocation[1]+1]==1:
@@ -246,6 +412,9 @@ if __name__ == "__main__":
     Screen = Canvas(root, width=width+2 , height=height+2 , bg='gray60')
     Screen.place(x=-2,y=-2)
 
+    MiniMap = Canvas(root,width=100,height=100,bg='gray60')
+    MiniMap.place(x=10,y=10)
+
     '''
     ############## Create screen Segments ##############
     '''
@@ -269,6 +438,28 @@ if __name__ == "__main__":
 
     RTC=Screen.create_polygon(500,0,400,100,500,100,fill='gray60',outline='gold',width=5,state='hidden')
     RBC=Screen.create_polygon(500,500,400,400,500,400,fill='gray60',outline='gold',width=5,state='hidden')
+
+    '''
+    ############## Create Map segments ##############
+    '''
+
+    C=[0,0,20,20]
+    for i in range(5):
+        for j in range(5):
+            MiniMapObjects.append(MiniMap.create_rectangle(C[0],C[1],C[2],C[3],fill='gold',state='hidden'))
+            C[0]+=20
+            C[2]+=20
+        C[0]=0
+        C[2]=20
+        C[1]+=20
+        C[3]+=20
+
+    MiniMap.itemconfig(MiniMapObjects[12],fill='red',state='normal')
+
+    NPointer = MiniMap.create_polygon(40,60,60,60,50,40,fill='darkred')
+    SPointer = MiniMap.create_polygon(40,40,60,40,50,60,fill='darkred',state='hidden')
+    EPointer = MiniMap.create_polygon(40,40,40,60,60,50,fill='darkred',state='hidden')
+    WPointer = MiniMap.create_polygon(60,40,60,60,40,50,fill='darkred',state='hidden')
 
     '''
     ############## Load Map ##############
@@ -302,8 +493,13 @@ if __name__ == "__main__":
             Screen.itemconfig(MLI,state='hidden')
             Screen.itemconfig(MRI,state='hidden')
 
+    MiniMap.place_forget()
+
+
 #binding
 root.bind('<Escape>',EXIT)
 root.bind('<Key>',Keybinding)
+
+
 
 mainloop()
