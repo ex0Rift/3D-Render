27f0aa14ec708e9deeaf2ab35dfecdc59f1cf417
ex0Rift <daviesnathanael45@gmail.com>
2025-06-05 12:57:48 +0100

    3

---
+++ RayCast.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+#vars
+
+width , height = 500,500
+
+Location =  [300,300]
+
+#constructors
+root = Tk()
+root.geometry(f'{width}x{height}+{ int(root.winfo_screenwidth() /2 - width / 2 )}+{ int(root.winfo_screenheight() /2 - height /2 )}')
+root.overrideredirect(True)
+
+#subs
+
+def EXIT(event=None):
+    root.destroy()
+    sys.exit()
+
+#mainloop
+
+if __name__ == "__main__":
+    print('[Run]Start here')
+
+    Screen = Canvas(root, width=width+2 , height=height+2 , bg='gray60')
+    Screen.place(x=-2,y=-2)
+
+#binding
+root.bind('<Escape>',EXIT)
+
+mainloop()
